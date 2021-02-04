6838493268f81f443f488a0db77f1f0271c6fd00
TANISH SAWANT <tanishsawant@rediffmail.com>
2021-02-04 10:14:16 +0530

    routing done!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Home from './components/Pages/Home'
 import React, {useEffect} from 'react'
 import {makeStyles} from '@material-ui/core/styles'
 import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
 const useStyles = makeStyles((theme)=>{
 
@@ -10,11 +12,18 @@ const useStyles = makeStyles((theme)=>{
 function App() {
   const classes = useStyles();
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
       </header>
-      <Home />
+      <Switch>
+        <Route path="/signin" component={SignIn}/>
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/" component={Home} />
+      </Switch>
+      
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+    return (
+        <div>
+            <h2>SignIn</h2>
+        </div>
+    )
+}
+
+export default SignIn
