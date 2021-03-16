d5d1f1d54fceb7d7446b1a4c86a9e7d4eb871cfa
TANISH SAWANT <tanishsawant@rediffmail.com>
2021-03-16 12:13:29 +0530

    firebase

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,27 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# cred={
+#   'apiKey': "AIzaSyDZiQxLHFbamQ7VRSW67fmzXoV7xUqhsac",
+#   'authDomain': "meetup-8405b.firebaseapp.com",
+#   'projectId': "meetup-8405b",
+#   'storageBucket': "meetup-8405b.appspot.com",
+#   'messagingSenderId': "435894963109",
+#   'appId': "1:435894963109:web:5516acf58b6334011ef64c",
+#   'measurementId': "G-W0DVPH0NKT"
+# }
+
+cred = credentials.Certificate("service.json")
+firebase_admin.initialize_app()
 
 app = FastAPI()
+
+db = firestore.client()
+
+db.collection(u'songs').add({'song': 'Imagine', 'artist': 'John Lennon'})
+
+@app.get('/')
+def greet():
+    return "Hello world!"
